5598534e7091332dd5fc6da76814c0ead505efcb
Kevin Phoenix <kevin@kphoenix.us>
2024-01-18 16:25:36 -0700

    Cleanup docker container in test case

--- python/tests/bootstrap/test_docker.py
+++ python/tests/bootstrap/test_docker.py
@@ -9,14 +9,16 @@ from binharness.types.target import Target
 
 def test_bootstrap_env_from_image(agent_binary_linux_host_arch: str) -> None:
     agent = bootstrap_env_from_image(agent_binary_linux_host_arch, "ubuntu:22.04")
+    try:
+        assert isinstance(agent, DockerAgent)
+        assert agent.get_environment_ids() == [0]
 
-    assert isinstance(agent, DockerAgent)
-    assert agent.get_environment_ids() == [0]
+        env = agent.get_environment(0)
+        target = Target(env, Path("/usr/bin/echo"), args=["hello world"])
+        proc = NullExecutor().run_target(target)
 
-    env = agent.get_environment(0)
-    target = Target(env, Path("/usr/bin/echo"), args=["hello world"])
-    proc = NullExecutor().run_target(target)
-
-    proc.wait()
-    assert proc.returncode == 0
-    assert proc.stdout.read() == b"hello world\n"
+        proc.wait()
+        assert proc.returncode == 0
+        assert proc.stdout.read() == b"hello world\n"
+    finally:
+        agent.container.stop()
