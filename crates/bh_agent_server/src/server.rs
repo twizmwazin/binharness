dd6e6ab52ff3ae7b8b32d78e0870d983186f9e0c
Kevin Phoenix <kevin@kphoenix.us>
2024-02-26 17:08:47 -0700

    Update dependencies

--- crates/bh_agent_server/src/server.rs
+++ crates/bh_agent_server/src/server.rs
@@ -1,4 +1,3 @@
-use std::future::{ready, Ready};
 use std::io::{Seek, SeekFrom, Write};
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -20,7 +19,7 @@ use crate::util::{read_generic, read_lines};
 macro_rules! check_env_id {
     ($env_id:expr) => {
         if $env_id != 0 {
-            return ready(Err(AgentError::InvalidEnvironmentId));
+            return Err(AgentError::InvalidEnvironmentId);
         }
     };
 }
@@ -40,224 +39,209 @@ impl BhAgentServer {
     }
 }
 
-#[tarpc::server]
 impl BhAgentService for BhAgentServer {
-    type GetEnvironmentsFut = Ready<Vec<EnvironmentId>>;
-    fn get_environments(self, _: Context) -> Self::GetEnvironmentsFut {
+    async fn get_environments(self, _: Context) -> Vec<EnvironmentId> {
         // Our implementation currently only supports the default environment
-        ready(vec![0])
+        vec![0]
     }
 
-    type GetTempdirFut = Ready<Result<String, AgentError>>;
-    fn get_tempdir(self, _: Context, env_id: EnvironmentId) -> Self::GetTempdirFut {
+    async fn get_tempdir(self, _: Context, env_id: EnvironmentId) -> Result<String, AgentError> {
         check_env_id!(env_id);
 
-        ready(Ok("/tmp".to_string())) // TODO: make configurable
+        Ok("/tmp".to_string()) // TODO: make configurable
     }
 
-    type RunCommandFut = Ready<Result<ProcessId, AgentError>>;
-    fn run_command(
+    async fn run_command(
         self,
         _: Context,
         env_id: EnvironmentId,
         config: RemotePOpenConfig,
-    ) -> Self::RunCommandFut {
+    ) -> Result<ProcessId, AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.run_command(config))
+        self.state.run_command(config)
     }
 
-    type GetProcessChannelFut = Ready<Result<FileId, AgentError>>;
-    fn get_process_channel(
+    async fn get_process_channel(
         self,
         _: Context,
         env_id: EnvironmentId,
         proc_id: ProcessId,
         channel: ProcessChannel,
-    ) -> Self::GetProcessChannelFut {
+    ) -> Result<FileId, AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.get_process_channel(&proc_id, channel))
+        self.state.get_process_channel(&proc_id, channel)
     }
 
-    type ProcessPollFut = Ready<Result<Option<u32>, AgentError>>;
-    fn process_poll(
+    async fn process_poll(
         self,
         _: Context,
         env_id: EnvironmentId,
         proc_id: ProcessId,
-    ) -> Self::ProcessPollFut {
+    ) -> Result<Option<u32>, AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.process_poll(&proc_id))
+        self.state.process_poll(&proc_id)
     }
 
-    type ProcessWaitFut = Ready<Result<bool, AgentError>>;
-    fn process_wait(
+    async fn process_wait(
         self,
         _: Context,
         env_id: EnvironmentId,
         proc_id: ProcessId,
         timeout: Option<u32>,
-    ) -> Self::ProcessWaitFut {
+    ) -> Result<bool, AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.process_wait(&proc_id, timeout))
+        self.state.process_wait(&proc_id, timeout)
     }
 
-    type ProcessReturncodeFut = Ready<Result<Option<u32>, AgentError>>;
-    fn process_returncode(
+    async fn process_returncode(
         self,
         _: Context,
         env_id: EnvironmentId,
         proc_id: ProcessId,
-    ) -> Self::ProcessReturncodeFut {
+    ) -> Result<Option<u32>, AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.process_exit_code(&proc_id))
+        self.state.process_exit_code(&proc_id)
     }
 
-    type FileOpenFut = Ready<Result<FileId, AgentError>>;
-    fn file_open(
+    async fn file_open(
         self,
         _: Context,
         env_id: EnvironmentId,
         path: String,
         mode: FileOpenMode,
         type_: FileOpenType,
-    ) -> Self::FileOpenFut {
+    ) -> Result<FileId, AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.open_path(path, mode, type_))
+        self.state.open_path(path, mode, type_)
     }
 
-    type FileCloseFut = Ready<Result<(), AgentError>>;
-    fn file_close(self, _: Context, env_id: EnvironmentId, fd: FileId) -> Self::FileCloseFut {
-        check_env_id!(env_id);
-
-        ready(self.state.close_file(&fd))
-    }
-
-    type FileIsClosedFut = Ready<Result<bool, AgentError>>;
-    fn file_is_closed(
+    async fn file_close(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
-    ) -> Self::FileIsClosedFut {
+    ) -> Result<(), AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.is_file_closed(&fd))
+        self.state.close_file(&fd)
     }
 
-    type FileIsReadableFut = Ready<Result<bool, AgentError>>;
-    fn file_is_readable(
+    async fn file_is_closed(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
-    ) -> Self::FileIsReadableFut {
+    ) -> Result<bool, AgentError> {
         check_env_id!(env_id);
 
-        ready(
-            self.state
-                .file_has_any_mode(&fd, &vec![FileOpenMode::Read, FileOpenMode::Update]),
-        )
+        self.state.is_file_closed(&fd)
     }
 
-    type FileReadFut = Ready<Result<Vec<u8>, AgentError>>;
-    fn file_read(
+    async fn file_is_readable(
+        self,
+        _: Context,
+        env_id: EnvironmentId,
+        fd: FileId,
+    ) -> Result<bool, AgentError> {
+        check_env_id!(env_id);
+
+        self.state
+            .file_has_any_mode(&fd, &vec![FileOpenMode::Read, FileOpenMode::Update])
+    }
+
+    async fn file_read(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
         num_bytes: Option<u32>,
-    ) -> Self::FileReadFut {
+    ) -> Result<Vec<u8>, AgentError> {
         check_env_id!(env_id);
 
-        ready(
-            self.state
-                .do_mut_operation(&fd, |file| {
-                    read_generic(file, num_bytes, self.state.file_type(&fd)?)
-                })
-                .and_then(|v| v.map_err(|e| IoError(e.to_string()))),
-        )
+        self.state
+            .do_mut_operation(&fd, |file| {
+                read_generic(file, num_bytes, self.state.file_type(&fd)?)
+            })
+            .and_then(|v| v.map_err(|e| IoError(e.to_string())))
     }
 
-    type FileReadLinesFut = Ready<Result<Vec<Vec<u8>>, AgentError>>;
-    fn file_read_lines(
+    async fn file_read_lines(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
         hint: u32,
-    ) -> Self::FileReadLinesFut {
+    ) -> Result<Vec<Vec<u8>>, AgentError> {
         check_env_id!(env_id);
 
         // TODO: support hint
-        ready(
-            self.state
-                .do_mut_operation(&fd, |file| {
-                    read_lines(file).map_err(|e| IoError(e.to_string()))
-                })
-                .and_then(|r| r),
-        )
+
+        self.state
+            .do_mut_operation(&fd, |file| {
+                read_lines(file).map_err(|e| IoError(e.to_string()))
+            })
+            .and_then(|r| r)
     }
 
-    type FileIsSeekableFut = Ready<Result<bool, AgentError>>;
-    fn file_is_seekable(
+    async fn file_is_seekable(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
-    ) -> Self::FileIsSeekableFut {
+    ) -> Result<bool, AgentError> {
         check_env_id!(env_id);
 
         todo!()
     }
 
-    type FileSeekFut = Ready<Result<(), AgentError>>;
-    fn file_seek(
+    async fn file_seek(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
         offset: i32,
         whence: i32,
-    ) -> Self::FileSeekFut {
+    ) -> Result<(), AgentError> {
         check_env_id!(env_id);
 
         let from = match whence {
             0 => SeekFrom::Start(offset as u64),
             1 => SeekFrom::Current(offset as i64),
             2 => SeekFrom::End(offset as i64),
-            _ => return ready(Err(AgentError::InvalidSeekWhence)),
+            _ => return Err(AgentError::InvalidSeekWhence),
         };
 
-        ready(
-            self.state
-                .do_mut_operation(&fd, |file| file.seek(from))
-                .map(|_| ()),
-        )
+        self.state
+            .do_mut_operation(&fd, |file| file.seek(from))
+            .map(|_| ())
     }
 
-    type FileTellFut = Ready<Result<i32, AgentError>>;
-    fn file_tell(self, _: Context, env_id: EnvironmentId, fd: FileId) -> Self::FileTellFut {
+    async fn file_tell(
+        self,
+        _: Context,
+        env_id: EnvironmentId,
+        fd: FileId,
+    ) -> Result<i32, AgentError> {
         check_env_id!(env_id);
 
         todo!()
     }
 
-    type FileIsWritableFut = Ready<Result<bool, AgentError>>;
-    fn file_is_writable(
+    async fn file_is_writable(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
-    ) -> Self::FileIsWritableFut {
+    ) -> Result<bool, AgentError> {
         check_env_id!(env_id);
 
-        ready(self.state.file_has_any_mode(
+        self.state.file_has_any_mode(
             &fd,
             &vec![
                 FileOpenMode::Write,
@@ -265,84 +249,87 @@ impl BhAgentService for BhAgentServer {
                 FileOpenMode::Update,
                 FileOpenMode::Append,
             ],
-        ))
+        )
     }
 
-    type FileWriteFut = Ready<Result<(), AgentError>>;
-    fn file_write(
+    async fn file_write(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
         data: Vec<u8>,
-    ) -> Self::FileWriteFut {
+    ) -> Result<(), AgentError> {
         check_env_id!(env_id);
 
-        ready(
-            self.state
-                .do_mut_operation(&fd, |file| file.write(&data))
-                .map(|_| ()),
-        )
+        self.state
+            .do_mut_operation(&fd, |file| file.write(&data))
+            .map(|_| ())
     }
 
-    type FileSetBlockingFut = Ready<Result<(), AgentError>>;
-    fn file_set_blocking(
+    async fn file_set_blocking(
         self,
         _: Context,
         env_id: EnvironmentId,
         fd: FileId,
         blocking: bool,
-    ) -> Self::FileSetBlockingFut {
+    ) -> Result<(), AgentError> {
         check_env_id!(env_id);
 
         #[cfg(target_family = "unix")]
-        return ready(
-            self.state
-                .do_mut_operation(&fd, |file| set_blocking(file, blocking))
-                .map(|_| ()),
-        );
+        return self
+            .state
+            .do_mut_operation(&fd, |file| set_blocking(file, blocking))
+            .map(|_| ());
 
         #[cfg(not(target_family = "unix"))]
-        return ready(Err(AgentError::UnsupportedPlatform));
+        return Err(AgentError::UnsupportedPlatform);
     }
 
-    type ChownFut = Ready<Result<(), AgentError>>;
-    fn chown(
+    async fn chown(
         self,
         _: Context,
         env_id: EnvironmentId,
         path: String,
         user: Option<UserId>,
         group: Option<UserId>,
-    ) -> Self::ChownFut {
+    ) -> Result<(), AgentError> {
         check_env_id!(env_id);
 
         #[cfg(target_family = "unix")]
-        return ready(chown(path, user, group));
+        return chown(path, user, group);
 
         #[cfg(not(target_family = "unix"))]
-        return ready(Err(AgentError::UnsupportedPlatform));
+        return Err(AgentError::UnsupportedPlatform);
     }
 
-    type ChmodFut = Ready<Result<(), AgentError>>;
-    fn chmod(self, _: Context, env_id: EnvironmentId, path: String, mode: u32) -> Self::ChmodFut {
+    async fn chmod(
+        self,
+        _: Context,
+        env_id: EnvironmentId,
+        path: String,
+        mode: u32,
+    ) -> Result<(), AgentError> {
         check_env_id!(env_id);
 
         #[cfg(target_family = "unix")]
-        return ready(chmod(path, mode));
+        return chmod(path, mode);
 
         #[cfg(not(target_family = "unix"))]
-        return ready(Err(AgentError::UnsupportedPlatform));
+        return Err(AgentError::UnsupportedPlatform);
     }
 
-    type StatFut = Ready<Result<bh_agent_common::FileStat, AgentError>>;
-    fn stat(self, _: Context, env_id: EnvironmentId, path: String) -> Self::StatFut {
+    async fn stat(
+        self,
+        _: Context,
+        env_id: EnvironmentId,
+        path: String,
+    ) -> Result<bh_agent_common::FileStat, AgentError> {
         check_env_id!(env_id);
 
         #[cfg(target_family = "unix")]
-        return ready(stat(path));
+        return stat(path);
 
         #[cfg(not(target_family = "unix"))]
-        return ready(Err(AgentError::UnsupportedPlatform));
+        return Err(AgentError::UnsupportedPlatform);
     }
 }
