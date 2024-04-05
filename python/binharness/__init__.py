8e3ef7aba3e9a73b38b3517bcf3b12f70e687b7e
Kevin Phoenix <kevin@kphoenix.us>
2024-04-04 17:53:14 -0700

    Bump version to 0.1.1

--- python/binharness/__init__.py
+++ python/binharness/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from binharness.agentenvironment import AgentConnection, AgentEnvironment
 from binharness.agentprovider import AgentProvider, DevEnvironmentAgentProvider
